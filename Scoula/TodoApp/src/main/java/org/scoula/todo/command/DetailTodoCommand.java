46639eee00ffd8ac091927d1778fb8831fc72e3f
S2gamzaS2 <ipnsohe@naver.com>
2024-07-20 00:37:50 +0900

    0702

---
+++ Scoula/TodoApp/src/main/java/org/scoula/todo/command/DetailTodoCommand.java
@@ -0,0 +1,26 @@
+package org.scoula.todo.command;
+
+import org.scoula.lib.cli.command.Command;
+import org.scoula.lib.cli.ui.Input;
+import org.scoula.todo.dao.TodoDao;
+import org.scoula.todo.domain.Todo;
+
+// 상세보기
+public class DetailTodoCommand implements Command {
+    TodoDao dao = TodoDao.getInstance(); // Dao
+
+    @Override
+    public void execute() {
+        int id = Input.getInt("Todo Id: "); // 에러1) id 입력 받을 때 - 숫자가 아닌 것을 입력했을 때
+        Todo todo = dao.getTodo(id); // 에러2) 존재하지 않는 id를 입력 했을 때 - Null - NullPointException 발생
+
+        System.out.println("[Todo 상세보기]-----------------------");
+        System.out.println("ID     : " + todo.getId());
+        System.out.println("제목    : " + todo.getTitle());
+        System.out.println("설명    : " + todo.getDescription());
+        System.out.println("완료여부 : " + todo.isDone());
+        System.out.println("등록일   : " + todo.getRegDate());
+        System.out.println("------------------------------------");
+        System.out.println();
+    }
+}
